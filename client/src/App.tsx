fe70793f20657e83c6cd2e22e5272aac2caf55a0
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2023-04-16 16:49:53 +0300

    Level 1 | cleanup app

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Babel from "./sandbox/Babel";
+
+function App() {
+  return (
+    <div className="App">
+      <Babel />
+    </div>
+  );
+}
+
+export default App;
