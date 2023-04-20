8047973fee54189b8f2493fc422aa33ac8f835b4
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2023-04-20 16:01:11 +0300

    Level 3 | MUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./App.css";
-import StyleModule from "./sandbox/components/styles/StyleModule";
-import Styles from "./sandbox/components/styles/Styles";
+import Card from "./cards/components/card/Card";
+// import StyleModule from "./sandbox/components/styles/StyleModule";
+// import Styles from "./sandbox/components/styles/Styles";
+// import MuiTypography from "./sandbox/mui-sandbox/data-display/MuiTypography";
+// import MuiDivider from "./sandbox/mui-sandbox/data-display/MuiDivider";
+// import MuiButton from "./sandbox/mui-sandbox/data-display/MuiButton";
 // import Logic from "./sandbox/components/Logic";
 // import StringInterpolation from "./sandbox/components/StringInterpolation";
 // import Template from "./sandbox/components/Template";
@@ -14,8 +18,12 @@ function App() {
       {/* <Template /> */}
       {/* <Logic />/ */}
       {/* <StringInterpolation /> */}
-      <Styles />
-      <StyleModule />
+      {/* <Styles /> */}
+      {/* <StyleModule /> */}
+      {/* <MuiTypography /> */}
+      {/* <MuiDivider />/ */}
+      {/* <MuiButton /> */}
+      <Card />
     </div>
   );
 }
---
+++ client/src/cards/components/card/Card.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import MuiCard from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+
+const Card = () => {
+  return (
+    <MuiCard sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="194"
+        image="https://cdn.pixabay.com/photo/2023/04/02/09/15/sempervivum-7894111_960_720.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </MuiCard>
+  );
+};
+
+export default Card;
